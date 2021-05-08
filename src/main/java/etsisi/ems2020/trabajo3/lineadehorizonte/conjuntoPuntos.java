b164d05a9a0bdaf94117c35d372cab86ac1f0eaf
Pablo Aguilera <pablo.aguilera.onieva@alumnos.upm.es>
2021-05-08 02:43:19 +0200

    sfgerg

---
+++ src/main/java/etsisi/ems2020/trabajo3/lineadehorizonte/conjuntoPuntos.java
@@ -0,0 +1,40 @@
+package etsisi.ems2020.trabajo3.lineadehorizonte;
+
+public class conjuntoPuntos {
+	private Punto p1;
+	private Punto p2;
+	private Punto paux;
+	
+	public conjuntoPuntos()
+	{
+		p1=new Punto();
+		p2=new Punto();
+		paux=new Punto();
+	}
+
+	public Punto getP1() {
+		return p1;
+	}
+
+	public void setP1(Punto p1) {
+		this.p1 = p1;
+	}
+
+	public Punto getP2() {
+		return p2;
+	}
+
+	public void setP2(Punto p2) {
+		this.p2 = p2;
+	}
+
+	public Punto getPaux() {
+		return paux;
+	}
+
+	public void setPaux(Punto paux) {
+		this.paux = paux;
+	}
+	
+	
+}
